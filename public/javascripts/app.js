ee1561bdc604708d6fa3e967fe1695ad42f04d6c
MissLast <1137546206@qq.com>
2015-04-09 13:11:57 +0800

    update views

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,55 +2,22 @@
  * Created by Cami on 15/3/21.
  */
 'use strict'
-
-// config the router and template
-/*angular.module('myApp', ['ui.router'])
-    .config(function ($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            .state('index', {
-                url: '/index',
-                views: {
-                    '': {
-                        templateUrl:'modules/client/index.html'
-                    },
-                    'topbar@index': {
-                        templateUrl:'modules/client/partials/topbar.html'
-                    },
-                    'footer@index': {
-                        templateUrl:'modules/client/partials/footer.html'
-                    }
-                }
-            })
-            .state('login', {
-                url: '/login',
-                views: {
-                    '': {
-                        templateUrl:'modules/client/login.html'
-                    },
-                    'topbar@index': {
-                        templateUrl:'modules/client/partials/topbar.html'
-                    },
-                    'footer@index': {
-                        templateUrl:'modules/client/partials/footer.html'
-                    }
-                }
-            });
-        $urlRouterProvider.otherwise('/index');
-    });*/
-
-angular.module('myApp', ['ngRoute'])
+angular.module('myApp', ['ngRoute', 'ui.bootstrap'])
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.
             when('/', {templateUrl: 'modules/client/index.html'}).
             when('/index', {templateUrl: 'modules/client/index.html'}).
             when('/admin', {templateUrl: 'modules/admin/index.html'}).
-            when('/login', {templateUrl: 'modules/client/login.html', controller: LoginCtrl }).
+            when('/login', {templateUrl: 'modules/client/login/login.html', controller:LoginCtrl}).
+            when('/login-successful', {templateUrl: 'modules/client/login/login-successful.html'}).
             when('/register', {templateUrl: 'modules/client/register/register.html', controller: RegisterCtrl}).
             when('/register/confirm', {templateUrl: 'modules/client/register/confirm.html'}).
-            when('/signup', {templateUrl: 'modules/client/signup/signup.html'}).
-            when('/signup/edit', {templateUrl: 'modules/client/signup/edit.html'}).
+            when('/signup', {templateUrl: 'modules/client/signup/signup.html', controller: SignupCtrl}).
+            when('/signup/edit', {templateUrl: 'modules/client/signup/edit.html', controller: EditSignupCtrl}).
+            when('/signup/signup-successful', {templateUrl: 'modules/client/signup/signup-successful.html'}).
             when('/profile', {templateUrl: 'modules/client/profile/index.html'}).
             when('/profile/basic', {templateUrl: 'modules/client/profile/basic.html'}).
+            when('/profile/info', {templateUrl: 'modules/client/profile/info.html'}).
             when('/profile/exam', {templateUrl: 'modules/client/profile/exam.html'}).
             when('/profile/record', {templateUrl: 'modules/client/profile/record.html'}).
             when('/profile/state', {templateUrl: 'modules/client/profile/state.html'}).
@@ -62,10 +29,3 @@ angular.module('myApp', ['ngRoute'])
             when('/admin/basic/statement', {templateUrl: 'modules/admin/views/basic/statement.html'}).
             otherwise({redirectTo: '/index'});
     }]);
-/*
-angular.module('adminApp',['ngRoute'])
-    .config(['$routeProvider', function($routeProvider) {
-        $routeProvider.
-            when('/admin', {}).
-            otherwise({redirectTo: '/admin'});
-    }])*/
